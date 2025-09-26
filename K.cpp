415ae14fd271415091f87604368dfa957b52b8a0
anussha2023 <anussha2023@gmail.com>
2025-09-26 21:57:13 +0600

    Create K.cpp

---
+++ K.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+ 
+    string digits;
+    cin >> digits;
+ 
+    long long sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += digits[i] - '0'; 
+    }
+    cout << sum << endl;
+ 
+    return 0;
+}
